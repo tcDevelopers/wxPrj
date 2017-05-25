e261b3b1e71e4a2ce02fc3d68b776c5582db1a32
meafe <570495405@qq.com>
2017-05-25 09:44:27 +0800

    123

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var meafe = require('utils/util_meafe.js');
 var fundebug = require('libs/fundebug.min.js');
 fundebug.apikey='a521937be063d9ff5bca4fe18c46e595582626055fe7bfdf2bc45fce0438b5ac';
-App({ 
+App({
   onLaunch: function () {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
