729d01e663ff81ad57473746cca867a34f334190
sxf <450034147@qq.com>
2017-05-26 10:45:56 +0800

    dele

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ fundebug.apikey='a521937be063d9ff5bca4fe18c46e595582626055fe7bfdf2bc45fce0438b5a
 App({
   onLaunch: function () {
     //调用API从本地缓存中获取数据
-    
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
